6d9089757e4437fe8f2361dc6ccc6036d25a5902
Yudam Jung <udamee1025@gmail.com>
2026-02-24 23:24:14 +0900

    final

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Character());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Zodiac App',
+      home: Character(),
+    );
   }
 }
